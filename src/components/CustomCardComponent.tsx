19430521b34d56db4ac983c8712a859ddb9c8e13
Laurohms <laurohmsantos@gmail.com>
2024-12-02 10:40:40 -0300

    feat: mouseOver card border change

--- src/components/CustomCardComponent.tsx
+++ src/components/CustomCardComponent.tsx
@@ -10,10 +10,12 @@ export const CustomCardComponent: React.FC<React.PropsWithChildren> = ({
           transition: "border 0.3s ease-in-out",
           border: "1px solid transparent",
         }}
-        onMouseEnter={(e) => (e.currentTarget.style.border = "1px solid white")}
-        onMouseLeave={(e) =>
-          (e.currentTarget.style.border = "1px solid transparent")
-        }
+        onMouseEnter={(e) => {
+          e.currentTarget.style.border = "1px solid cornFlowerBlue";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.border = " 1px solid transparent";
+        }}
       >
         {children}
       </Card>
